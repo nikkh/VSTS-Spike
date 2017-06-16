1374df1917083c5a60363207d4348214066bf52f
nick Hill <nick-9979@outlook.com>
2017-06-16 12:44:35 +0100

    huge amount fo changes

--- VSTS-Spike/Program.cs
+++ VSTS-Spike/Program.cs
@@ -12,6 +12,9 @@ using Microsoft.TeamFoundation.Core.WebApi.Types;
 using Microsoft.TeamFoundation.Core.WebApi;
 using Microsoft.VisualStudio.Services.Operations;
 using System.Threading;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+
 
 namespace VSTS_Spike
 {
@@ -20,51 +23,111 @@ namespace VSTS_Spike
         const string c_collectionUri = "https://nicks-ms-subscription.visualstudio.com/DefaultCollection";
         const string c_projectname = "xekina";
         const string c_reponame = "xekina";
+        static VssConnection v = null;
         static void Main(string[] args)
         {
             VssCredentials creds = new VssClientCredentials();
             creds.Storage = new VssClientCredentialStorage();
             VssConnection connection = new VssConnection(new Uri(c_collectionUri), creds);
+            v = connection;
             GitHttpClient gitClient = connection.GetClient<GitHttpClient>();
             var repo = gitClient.GetRepositoryAsync(c_projectname, c_reponame).Result;
-            WorkHttpClient w  =connection.GetClient<WorkHttpClient>();
+            // Get ptocess templates
             ProcessHttpClient processClient = connection.GetClient<ProcessHttpClient>();
             var processes = processClient.GetProcessesAsync().Result;
+
             
-            TeamContext tc = new TeamContext(c_projectname);
-            var teamIterations = w.GetTeamIterationsAsync(tc).Result;
-            var processConfiguration = w.GetProcessConfigurationAsync(c_projectname).Result;
-            var boards = w.GetBoardsAsync(tc).Result;
-            foreach (var board in boards)
-            {
-                Console.WriteLine(board.Name);
-            }
-           
+
 
             // and retrieve the corresponding project client 
             var projectHttpClient = connection.GetClient<ProjectHttpClient>();
 
             // then - same as above.. iterate over the project references (with a hard-coded pagination of the first 10 entries only)
-            foreach (var projectReference in projectHttpClient.GetProjects(top: 10, skip: 0).Result)
-            {
-                // and then get ahold of the actual project
-                var teamProject = projectHttpClient.GetProject(projectReference.Id.ToString()).Result;
-                var urlForTeamProject = ((ReferenceLink)teamProject.Links.Links["web"]).Href;
+            //foreach (var projectReference in projectHttpClient.GetProjects(top: 10, skip: 0).Result)
+            //{
+            //    // and then get ahold of the actual project
+            //    var teamProject = projectHttpClient.GetProject(projectReference.Id.ToString()).Result;
+            //    var urlForTeamProject = ((ReferenceLink)teamProject.Links.Links["web"]).Href;
                
-                Console.WriteLine("Team Project '{0}' (Id: {1}) at Web Url: '{2}' & API Url: '{3}'",
-                teamProject.Name,
-                teamProject.Id,
-                urlForTeamProject,
-                teamProject.Url);
+            //    Console.WriteLine("Team Project '{0}' (Id: {1}) at Web Url: '{2}' & API Url: '{3}'",
+            //    teamProject.Name,
+            //    teamProject.Id,
+            //    urlForTeamProject,
+            //    teamProject.Url);
                 
-            }
+            //}
             // return;
+           
+
+            // Create a new project
+            projectHttpClient = connection.GetClient<ProjectHttpClient>();
+            string projectName = CreateProject(connection, projectHttpClient);
+            var createdProject = projectHttpClient.GetProject(projectName).Result;
+
+            WorkHttpClient workClient = connection.GetClient<WorkHttpClient>();
+            TeamContext tc = new TeamContext(createdProject.Name);
+            var iterations = workClient.GetTeamIterationsAsync(tc).Result;
+            foreach (var item in iterations)
+            {
+                workClient.DeleteTeamIterationAsync(tc, item.Id).SyncResult();
+                Console.WriteLine("Deleting {0}", item.Name);
+            }
+            
+
+            
+            var processConfiguration = workClient.GetProcessConfigurationAsync(projectName).Result;
+
+            DateTime projectStartDate = new DateTime(2017, 07, 01);
+            DateTime startDate = projectStartDate;
+            DateTime endDate = startDate.AddMonths(1); ;
+            var node = AddIteration(createdProject.Id, "Discovery", startDate, endDate);
+            int discoveryNodeId = node.Id;
+            AddIteration(createdProject.Id, "Alpha", startDate, endDate);
+            AddIteration(createdProject.Id, "Inception", startDate, endDate, "Alpha");
+            AddIteration(createdProject.Id, "Beta", startDate, endDate);
+
+            WorkItemTrackingHttpClient workItemTrackingClient = v.GetClient<WorkItemTrackingHttpClient>();
+            workItemTrackingClient.DeleteClassificationNodeAsync(createdProject.Id, TreeStructureGroup.Iterations, "Iteration 1", discoveryNodeId).SyncResult();
+            workItemTrackingClient.DeleteClassificationNodeAsync(createdProject.Id, TreeStructureGroup.Iterations, "Iteration 2", discoveryNodeId).SyncResult();
+            workItemTrackingClient.DeleteClassificationNodeAsync(createdProject.Id, TreeStructureGroup.Iterations, "Iteration 3", discoveryNodeId).SyncResult();
+
+            var teamClient = connection.GetClient<TeamHttpClient>();
+            
+
+            Console.ReadLine();
+            return;
+        }
+
+        private static WorkItemClassificationNode AddIteration(Guid projectId, string iterationName, DateTime startDate, DateTime endDate, string path = null)
+        {
+            WorkItemClassificationNode iteration = new WorkItemClassificationNode
+            {
+                Name = iterationName,
+                StructureType = TreeNodeStructureType.Iteration
+            };
+
+            iteration.Attributes = new Dictionary<string, object>
+        {
+            {"startDate", startDate},
+            {"finishDate", endDate}
+        };
+
+            //add the iteration to the project
+
+            WorkItemTrackingHttpClient workItemTrackingClient = v.GetClient<WorkItemTrackingHttpClient>();
+            var x = workItemTrackingClient.CreateOrUpdateClassificationNodeAsync(iteration, projectId, TreeStructureGroup.Iterations, path).Result;
+            return x;
+        }
+
+        private static string CreateProject(VssConnection connection, ProjectHttpClient client)
+        {
+            
             // We can also create new projects, i.e. like this:
             var newTeamProjectToCreate = new TeamProject();
-            var somewhatRandomValueForProjectName = (int)(DateTime.UtcNow - DateTime.UtcNow.Date).TotalSeconds;
+            var somewhatRandomValueForProjectName = "CultureClubette";
 
             // mandatory information is name,
-            newTeamProjectToCreate.Name = $"Dummy Project {somewhatRandomValueForProjectName}";
+            newTeamProjectToCreate.Name = $"Project {somewhatRandomValueForProjectName}";
 
             // .. description
             newTeamProjectToCreate.Description = $"This is a dummy project";
@@ -76,7 +139,7 @@ namespace VSTS_Spike
         // particularly which version control the project shall use (as of writing 'TFVC' and 'Git' are available
         "versioncontrol", new Dictionary<string, string>()
 {
-{"sourceControlType", "TFVC"}
+{"sourceControlType", "Git"}
 }
 },
 {
@@ -90,7 +153,7 @@ namespace VSTS_Spike
 
             // because project creation takes some time on the server, the creation is queued and you'll get back a 
             // ticket / reference to the operation which you can use to track the progress and/or completion
-            var operationReference = projectHttpClient.QueueCreateProject(newTeamProjectToCreate).Result;
+            var operationReference = client.QueueCreateProject(newTeamProjectToCreate).Result;
 
             Console.WriteLine("Project '{0}' creation is '{1}'", newTeamProjectToCreate.Name, operationReference.Status);
 
@@ -113,9 +176,7 @@ namespace VSTS_Spike
             newTeamProjectToCreate.Name,
             projectCreationOperation.Status,
             projectCreationOperation.ResultMessage ?? "n.a.");
-
-            Console.ReadLine();
-            return;
+            return newTeamProjectToCreate.Name;
         }
     }
 }
